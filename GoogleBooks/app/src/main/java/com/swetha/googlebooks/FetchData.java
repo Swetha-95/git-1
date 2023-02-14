226c79a78495be8e60d6037a290067cc8073e837
Swetha <swetharubini5@gmail.com>
2023-02-14 10:32:23 +0530

    GoogleBooks-AsyncTask

--- GoogleBooks/app/src/main/java/com/swetha/googlebooks/FetchData.java
+++ GoogleBooks/app/src/main/java/com/swetha/googlebooks/FetchData.java
@@ -10,9 +10,7 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.ContentHandler;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FetchData extends AsyncTask<String, Void, String> {
